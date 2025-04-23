8136e9fe96a96f92ab7f8448fd16b28686ee02e6
Tristan Rush <145778062+Projex48@users.noreply.github.com>
2025-04-23 00:42:26 -0400

    Updated PSQL Connector
    Simplified PSQL connector, to be built back up at a later date

--- PostgreConnectorJS/psqlConnector.js
+++ PostgreConnectorJS/psqlConnector.js
@@ -1,48 +1,24 @@
-import { Pool } from 'pg';
-
+const { Pool } = require('pg');
+ 
 /**
- * PostgreSQL connector for Node.js.
+ * JS Connector for PostgreSQL
  * @author Tristan Rush
  */
-class PostgresClient {
-  /**
-   * Constructor for the PostgreSQL database connector
-   * @param pgUser The user to connect with
-   * @param pgHost The host to connect to
-   * @param pgDatabase The database name to connect to
-   * @param pgPassword The password for the user
-   * @param pgPort The port to connect to the database through
-   */
-  constructor(pgUser, pgHost, pgDatabase, pgPassword, pgPort) {
-    this.pool = new Pool({
-      user: pgUser,
-      host: pgHost,
-      databse: pgDatabase,
-      password: pgPassword,
-      port: pgPort || 5432,
-    });
-  }
 
-  /**
-   * Queries the pSQL database.
-   * @param sql The query to execute, parameters must use through placeholders
-   * @param params Params for the query, must be provided in an array
-   * @returns {Promise<*>} A promise for the result of the query process
-   * @throws {Error} If params are not provided in an array
-   */
-  async query(sql, params = []) {
-    if (!Array.isArray(params)) {
-      throw new Error('Params must be an array to avoid SQL injection risks.');
-    }
-    return this.pool.query(sql, params);
-  }
+/**
+ * Creates a psql pool
+ * @precondition A .env file must exist with the following args
+ * @env PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE
+ */
+const pool = new Pool({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+})
 
-  /**
-   * Disconnects from the database
-   */
-  async disconnect() {
-    await this.pool.end();
-  }
-}
-
-export default PostgresClient;
+export default pool
